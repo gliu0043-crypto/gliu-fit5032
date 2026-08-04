2060c5bed15daaca51ad4b2e541520ebb0b7bc1a
Guange Liu <gliu0043@student.monash.edu>
2026-08-04 12:39:35 +1000

    Initial FIT5032 repository

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-name">
+    <h2>My name is Guange Liu</h2>
+  </div>
+</template>
+
+<style scoped>
+.my-name {
+  margin-top: 16px;
+  padding: 12px;
+  border-left: 4px solid #42b883;
+  background-color: #f5f8f7;
+}
+
+.my-name h2 {
+  margin: 0;
+  color: #1b548c;
+  font-size: 1.4rem;
+}
+</style>
